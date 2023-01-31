3cd8391d37aef608d06c087a4ed5007551c695fe
snehafarkya <snhafarkya@gmail.com>
2023-01-31 21:29:54 +0530

    link added at work

--- src/component/Work.js
+++ src/component/Work.js
@@ -69,6 +69,26 @@ export default function Work() {
       <div className="grid md:grid-cols-3 grid-cols-1  place-items-center ">
         {workData}
       </div>
+      <div class="bg-indigo-900 text-center py-4 lg:px-4 w-max m-auto mt-20">
+          <div
+            class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+            role="alert"
+          >
+            <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+              New
+            </span>
+            <span class="font-semibold mr-2 text-left flex-auto">
+              See more projects<a href="https://www.github.com/snehafarkya"> here</a>
+            </span>
+            <svg
+              class="fill-current opacity-75 h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+            </svg>
+          </div>
+        </div>
     </div>
   );
 }
